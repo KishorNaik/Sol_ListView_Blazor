dd87a91b3632ec17105c01d5a0aaa12bf2733232
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-23 10:42:35 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/Demo/Components/ListView.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Sol_Demo.Models;
+
+namespace Sol_Demo.Pages.Demo.Components
+{
+    public partial class ListView<TItem>
+    {
+        [Parameter]
+        public IEnumerable<TItem> ItemSource { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> DataTemplate { get; set; }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Pages/Demo/PersonListComponent.razor.cs
@@ -0,0 +1,50 @@
+﻿using Sol_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class PersonListComponent
+    {
+        #region Private  Property
+
+        private List<PersonModel> PersonList { get; set; }
+
+        #endregion Private  Property
+
+        #region Private Method
+
+        private Task<IReadOnlyList<PersonModel>> GetPersonDataAsync()
+        {
+            return Task.Run<IReadOnlyList<PersonModel>>(() =>
+            {
+                var personList = new List<PersonModel>();
+                personList.Add(new PersonModel()
+                {
+                    FirstName = "Kishor",
+                    LastName = "Naik"
+                });
+                personList.Add(new PersonModel()
+                {
+                    FirstName = "Eshaan",
+                    LastName = "Naik"
+                });
+
+                return personList.AsReadOnly();
+            });
+        }
+
+        #endregion Private Method
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                PersonList = (await this.GetPersonDataAsync()).ToList();
+                base.StateHasChanged();
+            }
+        }
+    }
+}
